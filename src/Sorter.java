9fcd9de833ef4f1104986eda9d1380299eced8f1
IcePhoenix7 <151743271+IcePhoenix7@users.noreply.github.com>
2024-02-11 20:56:57 +0300

    Update Sorter.java
    gg

--- src/Sorter.java
+++ src/Sorter.java
@@ -19,4 +19,19 @@ public class Sorter {
             arr[i] = temp;
         }
     }
+
+     void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Shift elements of arr[0..i-1], that are greater than key, to one position ahead
+            // of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
